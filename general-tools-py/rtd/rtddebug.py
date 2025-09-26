d2cc0ae0e9e243568df93b3bca541a30109a87cc
thanasipantazides <thanasipantazides@gmail.com>
2025-09-26 14:42:42 -0500

    redoing rtddebug with better print stream

--- general-tools-py/rtd/rtddebug.py
+++ general-tools-py/rtd/rtddebug.py
@@ -11,41 +11,70 @@ mcast_group = "224.1.1.118"
 mcast_port = 9999
 
 # timestamping for display and logfile:
-default_time = datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
+default_time = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
 default_filename = os.path.join("log", "log_cooling_" + default_time + ".log")
 
 nperchip = 9
 nchip = 2
-nsensor = nperchip*nchip
+nsensor = nperchip * nchip
 
-def parse_rtd(id:int,data:bytes):
+
+def parse_rtd(id: int, data: bytes):
     blob = data[14:]
     channels = np.zeros(nperchip, dtype=np.uint8)
     errors = np.zeros(nperchip, dtype=np.uint8)
     values = np.zeros(nperchip, dtype=np.float32)
 
     ch = 0
-    sign_bit = 1<<24
+    sign_bit = 1 << 24
     for k in range(0, len(blob), 4):
-        ch = int(k/4)
+        ch = int(k / 4)
         channels[ch] = ch
         errors[ch] = blob[k]
-        temp_raw = int.from_bytes(blob[k+1:k+4], byteorder='big')
+        temp_raw = int.from_bytes(blob[k + 1 : k + 4], byteorder="big")
         if temp_raw & sign_bit:
-            temp_raw = temp_raw & ~sign_bit # clear the sign bit
-            temp_raw = -temp_raw            # negate the value
-        values[ch] = temp_raw / 1024.0      # move decimal 10 places left
+            temp_raw = temp_raw & ~sign_bit  # clear the sign bit
+            temp_raw = -temp_raw  # negate the value
+        values[ch] = temp_raw / 1024.0  # move decimal 10 places left
 
     return channels, errors, values
 
 
+def str_green(txt: str):
+    return "\033[92m" + txt + "\033[0m"
+
+
+def str_red(txt: str):
+    return "\033[91m" + txt + "\033[0m"
+
+
+def printstrip(channels, errors, values):
+    pad = "  "
+    twidth = 6
+    fmt = "{:" + str(twidth) + ".2f}"
+
+    strout = ""
+    for e, v in zip(errors, values):
+        if e == 1:
+            strout += str_green(fmt.format(v)) + pad
+        else:
+            strout += str_red(((twidth - 1) * " ") + "x") + pad
+        strout += "\n"
+
+    print(strout, end=None)
+
+
 if __name__ == "__main__":
     # join multicast group
-    local_recv_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+    local_recv_socket = socket.socket(
+        socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP
+    )
     local_recv_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
 
     local_recv_socket.bind((mcast_group, default_local_port))
-    mreq = struct.pack('4s4s', socket.inet_aton(mcast_group), socket.inet_aton(default_local_ip))
+    mreq = struct.pack(
+        "4s4s", socket.inet_aton(mcast_group), socket.inet_aton(default_local_ip)
+    )
     local_recv_socket.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
 
     print("joined!")
@@ -57,9 +86,10 @@ if __name__ == "__main__":
             if data[0] == 0x02 and data[5] == 0x12:
                 # print(data.hex())
                 sd = parse_rtd(data[8], data)
-                print("chip", data[8])
-                print("\terrors:", sd[1])
-                print("\tvalues:", sd[2])
+                # print("chip", data[8])
+                # print("\terrors:", sd[1])
+                # print("\tvalues:", sd[2])
+                printstrip(sd[0], sd[1], sd[2])
         except KeyboardInterrupt:
             break
 
